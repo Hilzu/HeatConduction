4eb04bae90f6feb75f5653f47f0a15e8bccfbedf
Santeri Hiltunen <hiltunen.santeri@gmail.com>
2011-10-27 21:37:27 +0300

    Added tests to main and modified array print

--- src/main.c
+++ src/main.c
@@ -7,9 +7,22 @@
 int main(int argc, char** argv)
 {
   if (is_default(argc, argv)) {
-    //replace
+    Array* arr = new_array(1000, 2000);
+    printf("new_array\n");
+    //print_arr(arr);
+    
+    initialize_array(arr, 0, 1000, 1000, 1000);
+    printf("init_array\n");
+    //print_arr(arr);
+    
+    double mean_temp = calculate_heatconduct(arr, 1000);
+    printf("calculate_array\n");
+    //print_arr(arr);
+    
+    printf("Mean temperature: %10.5f\n", mean_temp);
+    
   }
-  if (argc == 1)
+  else if (argc == 1)
   {
     print_help();
     return 0;
